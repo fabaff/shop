b0f4f0cefc6953017a6cc260799931cc1d6693af
Fabian Affolter <mail@fabian-affolter.ch>
2013-10-18 10:26:40 +0200

    add initial file for php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Webshop Pencil AG für Bleistifte">
+        <meta name="author" content="Fabian Affolter">
+        <title>Webshop Pencil AG | Home</title>
+
+        <link href="css/webshop.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- Header container-->
+        <div>
+            <div>
+              <!-- Logo and company name -->
+              <img src="logo/logo.png">
+              <h2>Webshop Pencil AG</h2>
+              <!-- Navigation -->
+              <ul class="navigation">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Produkte</a></li>
+                  <li><a href="#">Über uns</a></li>
+              </ul>
+              <!-- Breadcrumb -->
+              <ol class="breadcrumb">
+                  <li><a href="#">Ebene 1</a></li>
+                  <li><a href="#">Ebene 2</a></li>
+                  <li><a href="#">Ebene 3</a></li>
+              </ol>
+            </div>
+        </div>
+        <!-- Header container-->
+
+        <!-- Action container -->
+        <div class="action">
+            <h1>Wochen-Aktion</h1>
+            <p>Dies ist eine super Aktion. 10 Bleistifte für CHF 8.</p>
+        </div>
+        <!-- Action container -->
+
+        <!-- Selected products -->
+        <div class="row">
+            <p>Hier hat es zufällige Produkte...</p>
+        </div>
+        <!-- Selected products -->
+
+        <!-- Footer -->
+        <div class="footer">
+            <p>&copy; Pencil AG <?php echo date("Y") ?></p>
+        <!-- Footer -->
+        </div>
+    </body>
+</html>
