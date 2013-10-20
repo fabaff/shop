a53e5700933aadca3f87b1c9c40dba3c4c05849b
Fabian Affolter <mail@fabian-affolter.ch>
2013-10-20 16:22:26 +0200

    add initial product page as overview

---
+++ products.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Webshop Pencil AG für Bleistifte">
+    <meta name="author" content="Fabian Affolter">
+    <title>Webshop Pencil AG | Home</title>
+
+    <link href="css/webshop.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container" style="margin-top: 10px;">
+    <!-- Header -->
+      <div class="panel panel-default">
+        <div class="panel-body">
+        <!-- Logo and company name -->
+          <div>
+            <a class="brand-logo" href="index.html"></a>
+            <p class="brand-name">Webshop Pencil AG</p>
+          </div>
+        <!-- Navigation -->
+        <ul class="nav nav-tabs">
+          <li class=""><a href="index.php">Home</a></li>
+          <li classe="active"><a href="products.php">Produkte</a></li>
+          <li class=""><a href="#">Über uns</a></li>
+        </ul>
+        <!-- Breadcrumb not really needed the site is flat.
+        <ol class="breadcrumb">
+          <li><a href="#">Ebene 1</a></li>
+          <li><a href="#">Ebene 2</a></li>
+          <li><a href="#">Ebene 3</a></li>
+        </ol>-->
+    <!-- Header -->
+
+    <!-- Content -->
+        <!-- Action container aka Jumbotron Bootstrap
+        <div class="action">
+          <h1>Wochen-Aktion</h1>
+          <p>Dies ist eine super Aktion. 10 Bleistifte für CHF 8.</p>
+        </div>
+        Action container -->
+
+        <!-- Selected products -->
+        <div>
+            <table class="table table-striped">
+            <thead valign="bottom">
+            <tr><th class="head">Type</th>
+            <th class="head">Variant</th>
+            <th class="head">Color</th>
+            <th class="head">Hardness</th>
+            <th class="head">Price CHF</th>
+            </tr>
+            </thead>
+            <tbody valign="top">
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>HB</td>
+            <td>1</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>B</td>
+            <td>1</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>2B</td>
+            <td>1</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>gelb</td>
+            <td>F</td>
+            <td>1.20</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>gelb</td>
+            <td>H</td>
+            <td>1.20</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>gelb</td>
+            <td>2H</td>
+            <td>1.20</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>Gummi</td>
+            <td>rot</td>
+            <td>HB</td>
+            <td>1.10</td>
+            </tr>
+            <tr><td>Schreiner-Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>HB</td>
+            <td>1.80</td>
+            </tr>
+            <tr><td>Schreiner-Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>B</td>
+            <td>1.80</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>HB</td>
+            <td>0.5</td>
+            </tr>
+            <tr><td>Bleistift</td>
+            <td>Gummi</td>
+            <td>rot</td>
+            <td>HB</td>
+            <td>0.6</td>
+            </tr>
+            <tr><td>Künstler-Bleistift</td>
+            <td>Griff</td>
+            <td>gelb</td>
+            <td>2B</td>
+            <td>2</td>
+            </tr>
+            <tr><td>Künstler-Bleistift</td>
+            <td>Griff</td>
+            <td>rot</td>
+            <td>2B</td>
+            <td>2</td>
+            </tr>
+            <tr><td>Künstler-Bleistift</td>
+            <td>Griff</td>
+            <td>schwarz</td>
+            <td>2B</td>
+            <td>2</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>weiss</td>
+            <td>&nbsp;</td>
+            <td>4</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>blau</td>
+            <td>&nbsp;</td>
+            <td>4</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>gelb</td>
+            <td>&nbsp;</td>
+            <td>4</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>rot</td>
+            <td>&nbsp;</td>
+            <td>4</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>silber</td>
+            <td>&nbsp;</td>
+            <td>9</td>
+            </tr>
+            <tr><td>Minien-Bleistift</td>
+            <td>keine</td>
+            <td>schwarz</td>
+            <td>&nbsp;</td>
+            <td>9</td>
+            </tr>
+            </tbody>
+            </table>
+        </div>
+        <!-- Selected products -->
+
+        <!-- Content -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer">
+      <p>&copy; Pencil AG <?php echo date("Y") ?></p>
+    </div>
+    <!-- Footer -->
+  </body>
+</html>
