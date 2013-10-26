f9f119a55a0595927b3002dae37ea4d3e4a8fec6
Fabian Affolter <mail@fabian-affolter.ch>
2013-10-26 14:10:49 +0200

    add separate menu function

---
+++ menu.php
@@ -0,0 +1,22 @@
+<?php    
+    function menu() {
+        $part1 = '<ul class="nav nav-tabs">'."\n";
+        $part2 = '';
+        $part3 = '</ul>'."\n";
+        // Menu array with page title and assigned file
+        $menu = array('Home' => 'index.php',
+                      'Produkte' => 'products.php', 
+                      'Über uns' => 'about.php'
+            );
+        foreach ($menu as $label => $link) {
+            // For the CSS only the file name without / is needed
+            $url = trim($_SERVER['PHP_SELF'], '/');
+            if ($link == $url) {
+                $part2 = $part2."<li class='active'><a href='".$link."'>".$label."</a></li>\n";
+            } else {
+                $part2 = $part2."<li class=''><a href='".$link."'>".$label."</a></li>\n";
+            }
+        }
+        return $part1.$part2.$part3;
+    }
+?>
