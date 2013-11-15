229190352f25182919a3b7f742c416a9289764d9
Fabian Affolter <mail@fabian-affolter.ch>
2013-11-15 11:09:26 +0100

    add buy-me link

---
+++ product.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Webshop Pencil AG für Bleistifte">
+    <meta name="author" content="Fabian Affolter">
+    <title>Webshop Pencil AG | Produkt</title>
+
+    <link href="css/webshop.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container" style="margin-top: 10px;">
+    <!-- Header -->
+      <div class="panel panel-default">
+        <div class="panel-body">
+        <!-- Logo and company name -->
+            <?php 
+                require('header.php');
+                echo head();
+            ?>
+        <!-- Navigation -->
+            <?php 
+                require('menu.php');
+                echo menu();
+            ?>
+    <!-- Header -->
+    <!-- Content -->
+            <?php
+                $type=$_GET['type'];
+                $color=$_GET['color'];
+                $hardness=$_GET['hardness'];
+                $options=$_GET['options'];
+                echo "Ich bin ein $type, $color und $hardness. Ich habe $options Optionen.";
+            ?>
+    <!-- Content -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <?php 
+        require('footer.php');
+        echo foot();
+    ?>
+    <!-- Footer -->
+  </body>
+</html>
