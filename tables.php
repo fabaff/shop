5f313a725485c970be6293ea53ec2d0d21cd6169
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-04 21:29:00 +0100

    update design, use form for modifications

--- tables.php
+++ tables.php
@@ -5,9 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Webshop Pencil AG für Bleistifte">
     <meta name="author" content="Fabian Affolter">
-    <title>Webshop Pencil AG | Produkte</title>
+    <title>Webshop Pencil AG | Tables overview</title>
 
     <link href="css/webshop.css" rel="stylesheet">
+
+    <script type="text/javascript">
+        function confirmDelete() {
+            return window.confirm("Are you sure you want to delete this record?");
+        }
+    </script>
+
   </head>
 
   <body>
@@ -26,12 +33,13 @@
                 echo menu();
             ?>
     <!-- Header -->
-
     <!-- Content -->
         <div>
             <h3>Overview Table content</h3>
-            <!-- This page is more or less a simple test page for the database connection. -->
-            <!-- Keep in mind: The connection details are only in this page included (-> dbconnect.php). -->
+            <!-- This page was more or less a simple test page for the database connection. -->
+            <!-- Keep in mind: The connection details are only in this page included. 
+                 the db connection handling is done by including 'dbconnect.php' !
+            -->
             <?php
                 $connection = new mysqli("localhost", "root", "webshop", "webshop");
                 if (mysqli_connect_errno() == 0) {
@@ -40,13 +48,28 @@
                         echo "<h4>".ucfirst($section)."</h4>"."\n";
                         $sql = "SELECT * FROM $section";
                         $results = $connection->query($sql);
-                        echo mysqli_num_rows($results)." ".$section." entries found."."<br />"."\n";
-                        echo "<ul>"."\n";
+                        echo $results->num_rows." ".$section." entries found."."<br />"."\n";
+                        echo "<table>"."\n";
                         while ($result = $results->fetch_object()) {
-                            echo "<li>".$result->type."</li>";
+                            // The ugly way from a design point of view                            
+                            //echo "<a href=\"edit.php?table=$section&id=$result->id\">Edit entry</a>";
+                            //echo "<a href=\"scripts/delete-process.php?table=$section&id=$result->id\">Delete entry</a>";
+                            echo "<tr>"."\n";
+                            echo "<td>$result->type</td>"."\n";
+                            echo "<td><form action=\"edit.php\" method=\"POST\">
+                                <input type=\"hidden\" name=\"table\" value=\"$section\"/>
+                                <input type=\"hidden\" name=\"id\" value=\"$result->id\"/>
+                                <input type=\"submit\" name=\"Submit\" class=\"btn btn-xs btn-default\" value=\"Edit\">
+                            </form></td>"."\n";
+                            echo "<td><form action=\"scripts/delete-process.php\" onSubmit=\"return confirm('Are you sure to delete this entry?');\" method=\"POST\">
+                                <input type=\"hidden\" name=\"table\" value=\"$section\"/>
+                                <input type=\"hidden\" name=\"id\" value=\"$result->id\"/>
+                                <input type=\"submit\" name=\"Submit\" class=\"btn btn-xs btn-default\" value=\"Delete\">
+                            </form></td>"."\n";
+                            echo "</tr>"."\n";
                         }
-                        echo "</ul>"."\n";
-                        echo "<a href=\"add.php?table=$section\">Add new entry</a>";
+                        echo "</table>"."\n";
+                        echo "<a href=\"add.php?table=$section\">Add new entry to $section.</a>";
                     }
                 } else {
                     echo "Database connection error";
@@ -59,7 +82,6 @@
         </div>
       </div>
     </div>
-
     <!-- Footer -->
     <?php 
         require('scripts/footer.php');
