2cae2498c9fe12f0a40fbc024214edf02a92f76d
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-15 11:30:19 +0100

    add process script for entries

---
+++ scripts/add-process.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('../config/dbconnect.php');
+
+    // Get the parameters
+    $entry = $_POST['entry'];
+    $table = $_POST['table'];
+    // Create SQl query
+    $query = "INSERT INTO $table ( id, type ) VALUES ( NULL, '$entry' )";
+    // Run query and redirect to admin page
+    if ($connection->query($query)) {
+        echo "<script type=\"text/javascript\">
+                  alert(\"New entry $entry added successfully.\");
+                  window.location = \"../admin.php\"
+              </script>";
+    } else {
+        die("Failed: ".mysqli_error());
+    }
+    $connection->close();
+?>
