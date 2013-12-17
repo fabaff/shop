fc1ddd22755328406ce15172b24ac11d4bede6ff
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-17 22:12:53 +0100

    add helper script

---
+++ scripts/helpers.php
@@ -0,0 +1,51 @@
+<?php
+    /**
+     * Get the name of the full name of the page.
+     *
+     * @return Page name
+     */
+    function getFullPage() {
+        $this_page = $_SERVER['PHP_SELF'];
+        return $this_page;
+    }
+    /**
+     * Get the name of the name of the page.
+     *
+     * @return Page name
+     */
+    function getShortPage() {
+        return basename($_SERVER['PHP_SELF'], ".php");
+    }
+    /**
+     * Get the date (e.g. 10.10.2013).
+     *
+     * @return date
+     */
+    function getDateDMY() {
+        return date('d.m.Y');
+    }
+    /**
+     * Get the date (e. g. 2013-10-10). 
+     *
+     * @return date
+     */
+    function getDateYMD() {
+        return date('Y-m-d');
+    }
+    /**
+     * Get the year.
+     *
+     * @return date
+     */
+    function getDateY() {
+        return date('Y');
+    }
+    /**
+     * Get the time.
+     *
+     * @return time
+     */
+    function getTimeHMS() {
+        return date('H:i:s');
+    }
+?>
