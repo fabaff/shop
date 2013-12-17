c3c40a968f8aa470ff0063bf946bf7716fb8ea92
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-17 23:14:08 +0100

    use functions from helpers

--- scripts/footer.php
+++ scripts/footer.php
@@ -1,12 +1,10 @@
 <?php
+    include_once("scripts/helpers.php");
+    
     function foot() {
         $class1 = '<div class="footer">'."\n";
         $class2 = '</div>'."\n";
-        $text = "<p>&copy; Pencil AG ".getYear().'</p>'."\n";
+        $text = "<p>&copy; Pencil AG ".getDateY().'</p>'."\n";
         return $class1.$text.$class2;
-    }
-
-    function getYear(){
-        return date("Y");
     }
 ?>
