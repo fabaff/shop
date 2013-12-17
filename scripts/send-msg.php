d4e70f1f8cbf70c10971c05f44497a67fc028db3
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-17 22:11:37 +0100

    add new messaging features

---
+++ scripts/send-msg.php
@@ -0,0 +1,45 @@
+<?php
+    require_once('scripts/helpers.php');
+	/**
+	 * Send a message to a MQTT broker.
+     * 
+	 * @param String $subtopic
+	 * @param String $message
+	 */
+    function sendMsg($subtopic, $message) {
+        define('BROKER', 'localhost');
+        define('PORT', 1883);
+        define('CLIENT_ID', "webshop_" + getmypid());
+
+        $topic = "webshop";
+        $completeTopic = $topic."/".$subtopic;
+
+        $client = new Mosquitto\Client(CLIENT_ID);
+        $client->connect(BROKER, PORT, 60);
+        $client->publish($completeTopic, $message, 0, false);
+    }
+	/**
+	 * Send a default message to a MQTT broker.
+     * 
+	 * @param String $subtopic
+	 * @param String $page
+	 */
+    function defaultMsg($subtopic, $page) {
+        $date = getDateDMY();
+        $time = getTimeHMS();
+        $completeMessage = "Somebody visit the '".$page."' page at ".$date." on ".$time;
+        sendMsg($subtopic, $completeMessage);
+    }
+	/**
+	 * Send a custom message to a MQTT broker.
+     * 
+	 * @param String $subtopic
+	 * @param String $message
+	 */
+    function customMsg($subtopic, $message) {
+        $date = getDateDMY();
+        $time = getTimeHMS();
+        $completeMessage = $message." : ".$date." on ".$time;
+        sendMsg($subtopic, $completeMessage);
+    }
+?>
