bdebae9006f228950bae445894378736860715dd
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-18 01:09:39 +0100

    update

--- users.php
+++ users.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Webshop Pencil AG für Bleistifte">
     <meta name="author" content="Fabian Affolter">
-    <title>Webshop Pencil AG | Produkte</title>
+    <title>Webshop Pencil AG | Users</title>
 
     <link href="css/webshop.css" rel="stylesheet">
   </head>
@@ -17,38 +17,38 @@
         <div class="panel-body">
         <!-- Logo and company name -->
             <?php 
-                require('header.php');
+                require('scripts/header.php');
                 echo head();
             ?>
         <!-- Navigation -->
             <?php 
-                require('menu.php');
+                require('scripts/menu.php');
                 echo menu();
             ?>
     <!-- Header -->
 
     <!-- Content -->
-        <!-- Selected products -->
         <div>
-            <h3>Übersicht Benutzer</h3>
+            <h3>Overview Users</h3>
             <?php
                 require_once('config/dbconnect.php');
-                if (mysqli_connect_errno() == 0) {                  
+                if (mysqli_connect_errno() == 0) {
                     // Get users
-                    $sql_users = "SELECT * FROM login";
-                    $res_users = $connection->query($sql_users);
-                    echo mysqli_num_rows($res_users)." users entries found."."<br />"."\n";
+                    $sql = "SELECT * FROM login";
+                    $results = $connection->query($sql);
+                    echo mysqli_num_rows($results)." users entries found."."<br />"."\n";
                     echo "<ul>"."\n";
-                    while ($s_users = mysqli_fetch_assoc($res_users)) {
-                        echo "<li>".$s_users["username"]."</li>";
+                    while ($result = $results->fetch_object()) {
+                        echo "<li>".$result->username."</li>";
                     }
                     echo "</ul>"."\n";
-                    mysqli_close();
+                    $results->close();
+                } else {
+                    echo "Database connection error";
                 }
-                else echo "Database connection error";
+                $connection->close();
             ?>
         </div>
-        <!-- Selected products -->
         <!-- Content -->
         </div>
       </div>
@@ -56,7 +56,7 @@
 
     <!-- Footer -->
     <?php 
-        require('footer.php');
+        require('scripts/footer.php');
         echo foot();
     ?>
     <!-- Footer -->
