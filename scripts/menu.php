b8a4178a2f883fa044a490e97a9bab69654fa223
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-28 09:53:13 +0100

    fix language

--- scripts/menu.php
+++ scripts/menu.php
@@ -5,11 +5,11 @@
         $part3 = '</ul>'."\n";
         // Menu array with page title and assigned file
         $menu = array('Home' => 'index.php',
-                      'Produkte' => 'products.php', 
-                      'Über uns' => 'about.php'
+                      'Products' => 'products.php', 
+                      'About' => 'about.php'
             );
         foreach ($menu as $label => $link) {
-            // For the CSS only the file name without / is needed
+            // For the CSS, only the file name without / is needed
             $url = trim($_SERVER['PHP_SELF'], '/');
             if ($link == $url) {
                 $part2 = $part2."<li class='active'><a href='".$link."'>".$label."</a></li>\n";
