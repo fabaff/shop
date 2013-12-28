fcc8173ba5d80ccb4803c0b29625edd15f5aa2b9
Fabian Affolter <mail@fabian-affolter.ch>
2013-12-28 10:20:02 +0100

    add page for managing new products

---
+++ add-product.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Webshop Pencil AG für Bleistifte">
+    <meta name="author" content="Fabian Affolter">
+    <title>Webshop Pencil AG | Add product</title>
+
+    <link href="css/webshop.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="container" style="margin-top: 10px;">
+    <!-- Header -->
+      <div class="panel panel-default">
+        <div class="panel-body">
+        <!-- Logo and company name -->
+            <?php 
+                require('scripts/header.php');
+                echo head();
+            ?>
+        <!-- Navigation -->
+            <?php 
+                require('scripts/menu.php');
+                echo menu();
+            ?>
+    <!-- Header -->
+
+    <!-- Content -->
+        <div>
+            <form name="add" action="scripts/add-product-process.php" method="post">
+            <table width="510" border="0">
+            <tr>
+                <td colspan="2"></td>
+            </tr>
+            <tr>
+                <td>Product name:</td>
+                <td><input class="form-control" type="text" name="pname" maxlength="20" /></td>
+            </tr>
+            <tr>
+                <td>Type:</td>
+                <td><select class="form-control input-sm" name="ptype">
+									<option></option>
+            <?php
+                require_once('config/dbconnect.php');
+                $table = "pencils";
+                $sql = "SELECT * FROM $table";
+                $results = $connection->query($sql);
+                while ($result = $results->fetch_object()) {
+                    echo '<option value='.$result->id.'>'.$result->type.'</option>';
+                }
+            ?></select>
+</td>
+            </tr>
+            <tr>
+                <td>Option:</td>
+                <td><select class="form-control input-sm" name="poption">
+									<option></option>
+									            <?php
+                require_once('config/dbconnect.php');
+                $table = "options";
+                $sql = "SELECT * FROM $table";
+                $results = $connection->query($sql);
+                while ($result = $results->fetch_object()) {
+                    echo '<option value='.$result->id.'>'.$result->type.'</option>';
+                }
+            ?>
+								</select></td>
+            </tr>
+            <tr>
+                <td>Color:</td>
+                <td>
+<select class="form-control input-sm" name="color">
+									<option></option>
+            <?php
+                require_once('config/dbconnect.php');
+                $table = "colors";
+                $sql = "SELECT * FROM $table";
+                $results = $connection->query($sql);
+                while ($result = $results->fetch_object()) {
+                    echo '<option value='.$result->id.'>'.$result->type.'</option>';
+                }
+            ?>
+								</select>
+</td>
+            </tr>
+            <tr>
+                <td>Hardness:</td>
+                <td><select class="form-control input-sm" name="hardness">
+									<option></option>
+            <?php
+                require_once('config/dbconnect.php');
+                $table = "hardness";
+                $sql = "SELECT * FROM $table";
+                $results = $connection->query($sql);
+                while ($result = $results->fetch_object()) {
+                    echo '<option value='.$result->id.'>'.$result->type.'</option>';
+                }
+            ?>
+								</select>
+            </tr>
+            <tr>
+                <td>Price (CHF):</td>
+                <td><input class="form-control" type="text" name="price" maxlength="10" /></td>
+            </tr>
+
+            <tr>
+                <td>&nbsp;</td>
+                <td><button class="btn btn-default" type="submit" value="Add">Add entry</button></td>
+            </tr>
+            </table>
+            </form>
+        
+        </div>
+        <!-- Selected products -->
+        <!-- Content -->
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <?php 
+        require('scripts/footer.php');
+        echo foot();
+    ?>
+    <!-- Footer -->
+  </body>
+</html>
---
+++ scripts/add-product-process.php
@@ -0,0 +1,37 @@
+<?php
+    require_once('../config/dbconnect.php');
+    require_once('helpers.php');
+    //Get the parameters
+    $pname = $_POST['pname'];
+    $ptype = $_POST['ptype'];
+    $poption = $_POST['poption'];
+    $color = $_POST['color'];
+    $hardness = $_POST['hardness'];
+    $price = $_POST['price'];
+    $adate = getDateYMD();
+
+    //Debug
+    /*echo$pname;
+    echo$pptype;
+    echo$poption;
+    echo$color;
+    echo$hardness;
+    echo$price;
+    echo$adate;
+    */
+    $table = 'products';
+
+    // Create SQLquery
+    $query = "INSERT INTO $table (`id`, `pname`, `ptype`, `poption`, `color`, `hardness`, `price`, `adate`) VALUES ( NULL, '$pname', $ptype, $poption, $color, $hardness, $price, '$adate')";
+    echo $query;
+    //Run query and redirect to admin page
+    if ($connection->query($query)) {
+        echo "<script type=\"text/javascript\">
+                  alert(\"New product '$pname' added successfully.\");
+                  window.location = \"../admin.php\"
+              </script>";
+    } else {
+        die("Failed: ".mysqli_error());
+    }
+    $connection->close();
+?>
