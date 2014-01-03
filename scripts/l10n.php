5595412ef8311b8cc041fdf8d2024dbcba9ef596
Fabian Affolter <mail@fabian-affolter.ch>
2014-01-03 22:39:00 +0100

    move to sep. file

---
+++ scripts/l10n.php
@@ -0,0 +1,10 @@
+<?php
+    $locale = $_SESSION["SESSION_LANG"];
+
+    putenv('LANG='.$locale);
+    setlocale(LC_ALL, $locale);
+    $domain = 'webshop';
+    bindtextdomain($domain, 'locale');
+    bind_textdomain_codeset($domain, 'UTF-8');
+    textdomain($domain);
+?>
