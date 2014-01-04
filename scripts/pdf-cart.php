3f23edf95df15b765450e976d2a254a89bc39ad8
Fabian Affolter <mail@fabian-affolter.ch>
2014-01-04 11:19:43 +0100

    add pdf output (cart and product)

---
+++ scripts/pdf-cart.php
@@ -0,0 +1,94 @@
+<?php
+    define('FPDF_FONTPATH','../font/');
+    require('fpdf.php');
+    include('../config/dbconnect.php');
+    require_once('helpers.php');
+    include('ShoppingCart.php');
+	session_start();
+
+	$cartArray = $_SESSION["cart"]->getArray();
+    // Something to implement
+	$firstName = "First name of the customer";
+	$lastName = "Last name of the customer";
+    $date = getDateYMD();
+
+    // Instanciation of inherited class
+    $pdf = new FPDF('p', 'mm', 'A4');
+    $pdf->AliasNbPages();
+    $pdf->SetFont('Times', '', 12);
+    $pdf->AddPage();
+
+    // Header
+    $pdf->SetX(10);
+    $pdf->SetFont('Times', '', 10);
+    $pdf->Cell(80, 0, 'Pencil AG | Musterstrasse 1 | 3000 Bern | pencil@webshop');
+    $pdf->Ln(1);
+    $pdf->SetFont('Times', 'B', 16);
+    $pdf->Ln(10);
+    $pdf->Image('../images/logo.png', 20, 20, 20);
+    $pdf->Cell(180 , 20, 'Order overview'.'  '.$date, 0, 0, 'C');
+    $pdf->Ln(40);
+
+    // Address area
+    $pdf->SetX(10);
+    $pdf->SetY(50);
+    $pdf->SetFont('Times', '', 12);
+    $pdf->Cell(80, 6, $firstName." ".$lastName, '');
+    $pdf->Ln();
+    $pdf->Cell(80, 6, 'Street and nuber', '');
+    $pdf->Ln();
+    $pdf->Cell(80, 6, 'Postal code and city', '');
+    $pdf->Ln();
+
+    // Set some initial values
+    $y_axis_initial = 75;
+    $row_height = 6;
+    $max = 25;
+    $i = 0;
+    $totalp = 0;
+
+    // Create table
+    $pdf->SetFillColor(255, 255, 255);
+    $pdf->SetFont('Times', 'B', 12);
+    $pdf->SetY($y_axis_initial);
+    $pdf->SetX(25);
+    $pdf->Cell(10, 6, '#', 1, 0, 'C', 1);
+    $pdf->Cell(30, 6, 'Name', 1, 0, 'L', 1);
+    $pdf->Cell(80, 6, 'Description', 1, 0, 'L', 1);
+    $pdf->Cell(10, 6, 'Qty', 1, 0, 'C', 1);
+    $pdf->Cell(20, 6, 'Price', 1, 0, 'C', 1);
+    $pdf->Cell(20, 6, 'Subtotal', 1, 0, 'C', 1);
+
+    $y_axis = $y_axis_initial + $row_height;
+    $connection = new mysqli("localhost", "root", "webshop", "webshop");
+    $total = 0;
+foreach ($cartArray as $art=>$qty) {
+    $sql = "SELECT * FROM products WHERE id='$art'";
+    $results = $connection->query($sql);
+    $result = $results->fetch_object();
+    $pname = $result->pname;
+    $description = $result->pdesc;
+    $price = $result->price;
+    $subtotal = $qty * $price;
+    $totalp = $totalp + $subtotal;
+
+    $pdf->SetFont('Times', '', 10);
+    $pdf->SetY($y_axis);
+    $pdf->SetX(25);
+    $pdf->Cell(10, 6, $i, 1, 0, 'C', 1);
+    $pdf->Cell(30, 6, $pname, 1, 0, 'L', 1);
+    $pdf->Cell(80, 6, $description, 1, 0, 'L', 1);
+    $pdf->Cell(10, 6, $qty, 1, 0, 'C' , 1);
+    $pdf->Cell(20, 6, $price, 1, 0, 'C' , 1);
+    $pdf->Cell(20, 6, $subtotal, 1, 0, 'C', 1);
+
+    // Go to next product
+    $y_axis = $y_axis + $row_height;
+    $i = $i + 1;
+}
+    $pdf->Ln(2);
+    $pdf->SetFont('Times', 'B', 12);
+    $pdf->SetY($y_axis + 5);
+    $pdf->Cell(180, 5, 'Total price: '.$totalp.' CHF', 0, 0, 'R');
+    $pdf->Output('order.pdf', 'I');
+?>
---
+++ scripts/pdf-spec.php
@@ -0,0 +1,57 @@
+<?php
+    define('FPDF_FONTPATH','../font/');
+    require('fpdf.php');
+    include('../config/dbconnect.php');
+    require_once('helpers.php');
+
+    // Get the parameter
+    $id = $_GET['id'];
+    // Create SQL query
+    // TODO: Fix this!
+    $connection = new mysqli("localhost", "root", "webshop", "webshop");
+    $sql = "SELECT products.pname AS name,
+                products.pdesc AS description,
+                pencils.type AS type,
+                options.type AS option,
+                colors.type AS color,
+                hardness.type AS hardness,
+                products.price AS price
+                FROM products 
+                    JOIN pencils ON products.ptype=pencils.id
+                    JOIN options ON products.poption=options.id
+                    JOIN colors ON products.color=colors.id
+                    JOIN hardness ON products.hardness=hardness.id
+                    WHERE products.id=$id";
+    $results = $connection->query($sql);
+    $result = $results->fetch_object();
+
+    // Instanciation of inherited class
+    $pdf = new FPDF('l', 'mm', 'A5');
+    $pdf->AliasNbPages();
+    $pdf->SetFont('Times', '', 12);
+    $pdf->AddPage();
+
+    // Header
+    $pdf->SetX(10);
+    $pdf->SetFont('Times', '', 10);
+    $pdf->Cell(80, 0, 'Pencil AG | Musterstrasse 1 | 3000 Bern | pencil@webshop');
+    $pdf->Ln(1);
+    $pdf->SetFont('Times', 'B', 16);
+    $pdf->Ln(10);
+    $pdf->Image('../images/logo.png', 20, 20, 20);
+    $pdf->Cell(180 , 20, 'Product specification sheet', 0, 0, 'C');
+    $pdf->Ln(40);
+
+    // Now show the 3 columns with the data
+    $pdf->SetFont('Times', '', 12);
+    $pdf->Cell(20, 6, $result->name, 0);
+    $pdf->Cell(100, 6, $result->description, 0);
+    $pdf->Cell(100, 6, $result->type, 0);
+
+    // Footer
+    $pdf->SetY(115);
+    $pdf->SetFont('Times', '', 8);
+    $pdf->Cell(0, 10, 'Page '.$pdf->PageNo().' of {nb} page(s)', 0, 0, 'C');
+
+    $pdf->Output('product-specifications.pdf', 'I');
+?>
