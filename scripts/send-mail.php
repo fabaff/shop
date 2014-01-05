a0c2d6b4055c0eb72d806e783e0fad44a43a1b83
Fabian Affolter <mail@fabian-affolter.ch>
2014-01-06 00:15:37 +0100

    add comments

--- scripts/send-mail.php
+++ scripts/send-mail.php
@@ -1,13 +1,24 @@
 <?php
-    // SE Linux is the show stopper here...check the host's configuration
-    //  setsebool -P httpd_can_network_connect=1
-    //  setsebool -P httpd_can_sendmail=1
-    function sendMail() {
-        $to = "root@Localhost";
-        $subj = "Hello";
-        $mess = "Hi Customer,\nYour order will be shipped soon!\nPencil Webshop";
+    /* Only root@localhost is a valid recipient. The postfix configuration is
+     * not checked yet and perhaps will never be. No mails are sent to the
+     * outside world.
+     * For testing:
+     * mail('root@localhost', 'Test Message', 'Test message body.');
+    */
+
+	/**
+	 * Send a message over the local SMTP server to a recipient.
+     * 
+	 * @param String $to
+	 * @param String $subtopic
+	 * @param String $message
+	 */
+    function sendMail($to, $subj, $message) {
+        $to = "root@localhost";
+        $subj = "Pencil webshop order";
+        $message = "Hi Customer,\nYour order will be shipped soon!\nPencil Webshop";
         $from = "From:webshop@pencil.webshop";
 
-        mail($to, $subj, $mess, $from."\r\n");
+        mail($to, $subj, $message, $from."\r\n");
     }
 ?>
