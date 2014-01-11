be1acf94077d0deedfab5c72f405a05a0976353b
Fabian Affolter <mail@fabian-affolter.ch>
2014-01-11 13:33:57 +0100

    update l10n

--- l10n.php
+++ l10n.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+    require_once('scripts/helpers.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,6 +32,13 @@
     <!-- Header -->
     <!-- Content -->
         <div>
+            <h3>Get browser language</h3>
+            <p>
+            <?php
+                $language = explode(',' , $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+                echo "Your browser is using: ".$language[0];
+            ?>
+            </p>
             <h3>Check availability of L10n support</h3>
             <p>
             <?php
@@ -42,7 +50,7 @@
                 }
             ?>
             </p>
-            <h3>Check translations</h3>
+            <h3>Check translations (hard-coded)</h3>
             <p>
             <?php
                 $locale = 'de_CH';
@@ -67,43 +75,67 @@
             ?>
             </p>
             <h3>Switch language</h3>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-				<div class="col-md-8">
-					<div class="col-md-4">
-						<label class="col-lg-10 control-label">Language selection:</label>
-					</div>
-
-					<div class="col-lg-2">
-						<select class="form-control input-sm" name="lang">
-							<option value="de_CH">de</option>
-                            <option value="fr_FR">fr</option>
-                            <option value="en_US">en</option>
-						</select>
-					</div>
-
-					<div class="col-lg-2">
-						<button class="btn btn-default" type="submit">Switch language</button>
-					</div>
-				</div>
-            </form>
-            <br/>
-            <br/>
+            <!-- Dropdown is too big
             <?php
-                echo "Selected language: ".$_POST['lang'];
-                echo "<br/>"."\n";
-                $_SESSION["SESSION_LANG"] = $_POST['lang'];
-                echo "Language in session: ".$_SESSION["SESSION_LANG"];
-                echo "<br/>"."\n";
-                echo "<br/>"."\n";
+            if (isset($_POST['lang-selection'])) {
+                            $locale = $_POST['lang'];
+                            putenv('LANG='.$locale);
+                            setlocale(LC_ALL, $locale);
+                            $domain = 'webshop';
+                            bindtextdomain($domain, 'locale');
+                            bind_textdomain_codeset($domain, 'UTF-8');
+                            textdomain($domain);
+                            
+                            echo 'Source : This is a simple test. ==> Target : ';
+                            echo _('This is a simple test');
+                            echo ' ('.$locale.')';
+                            echo '<br/>';
+                            echo 'Source : Good morning ==> Target : ';
+                            echo _('Good morning');
+                            echo ' ('.$locale.')';
+                            echo '<br/>';
+                            echo 'Source : Good night ==> Target : ';
+                            echo _('Good night');
+                            echo ' ('.$locale.')';
+            } else {
+                      $form = "<form action=\"l10n.php\" method=\"POST\" name=\"lang-selection\">
+				            <div class=\"col-md-8\">
+					            <div class=\"col-md-4\">
+						            <label class=\"col-lg-10 control-label\">Language selection:</label>
+					            </div>
+
+					            <div class=\"col-lg-2\">
+						            <select class=\"form-control input-sm\" name=\"lang\">
+							            <option value=\"de_CH\">de</option>
+                                        <option value=\"fr_FR\">fr</option>
+                                        <option value=\"en_US\">en</option>
+						            </select>
+					            </div>
+
+					            <div class=\"col-lg-2\">
+						            <button class=\"btn btn-default\" type=\"submit\">Switch language</button>
+					            </div>
+				            </div>
+                        </form>
+                        <br/>
+                        <br/>";
+                echo $form."\n";
+            }
+            ?>
+            <br/>-->
+            <?php
+            if (isset($_POST['lang'])) {
+                
+                echo 'The selection was: '. $_POST['lang'].'<br/>';
 
                 $locale = $_POST['lang'];
+                $_SESSION["SESSION_LANG"] = $locale;
                 putenv('LANG='.$locale);
                 setlocale(LC_ALL, $locale);
                 $domain = 'webshop';
                 bindtextdomain($domain, 'locale');
                 bind_textdomain_codeset($domain, 'UTF-8');
                 textdomain($domain);
-                
                 echo 'Source : This is a simple test. ==> Target : ';
                 echo _('This is a simple test');
                 echo ' ('.$locale.')';
@@ -115,44 +147,22 @@
                 echo 'Source : Good night ==> Target : ';
                 echo _('Good night');
                 echo ' ('.$locale.')';
-            ?>
-            <br/>
-            <br/>
-            <?php
+                echo '<br/>';
+} else {
                 // All available languages
                 $languages = array('de_CH', 'fr_FR', 'en_US');
                 foreach ($languages as $lang) {
-                    echo "<a href=\"javascript:document.".$lang.".submit();\">".substr($lang, 0, 2)."</a> "."\n";
+                    echo "<a href=\"javascript:document.".$lang.".submit(); \">".substr($lang, 0, 2)."</a> "."\n";
                 }
                 // Create hidden form for language selection
                 foreach ($languages as $lang) {
-                    echo "<form action=".$_SERVER['PHP_SELF']." method=\"GET\" name=\"$lang\">"."\n";
+                    echo "<form action=".$_SERVER['PHP_SELF']." method=\"POST\" name=\"$lang\">"."\n";
                     echo "<input type=\"hidden\" name=\"lang\" value=\"$lang\"/>"."\n";
                     echo "</form>"."\n";
                 }
-                $_SESSION["SESSION_LANG"] = $_GET['lang'];
-                $locale = $_GET['lang'];
-                putenv('LANG='.$locale);
-                setlocale(LC_ALL, $locale);
-                $domain = 'webshop';
-                bindtextdomain($domain, 'locale');
-                bind_textdomain_codeset($domain, 'UTF-8');
-                textdomain($domain);
-
-                echo 'Source : This is a simple test. ==> Target : ';
-                echo _('This is a simple test');
-                echo ' ('.$locale.')';
-                echo '<br/>';
-                echo 'Source : Good morning ==> Target : ';
-                echo _('Good morning');
-                echo ' ('.$locale.')';
-                echo '<br/>';
-                echo 'Source : Good night ==> Target : ';
-                echo _('Good night');
-                echo ' ('.$locale.')';
-            ?>
+}
+?>
             <br/>
-
         </div>
         <!-- Content -->
         </div>
